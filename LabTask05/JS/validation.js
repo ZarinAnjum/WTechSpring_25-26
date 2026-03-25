42f1efa89595daad8c0be343b24e6a4c72557ac0
ZarinAnjum <zarinanjum06@gmail.com>
2026-03-25 21:53:03 +0600

    eighth

--- LabTask05/JS/validation.js
+++ LabTask05/JS/validation.js
@@ -1,1 +1,10 @@
 console.log("Connected");
+
+function analyzeText(){
+
+let text = document.getElementById("textInput").value;
+if(text.trim()===""){
+    alert("Enter text first");
+    return;
+}
+}
