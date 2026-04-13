a6e891206dce16825952089800f789ec7c207210
ZarinAnjum <zarinanjum06@gmail.com>
2026-04-14 01:02:43 +0600

    page done

--- FINAL TERM/LAB1/View/Registration.php
+++ FINAL TERM/LAB1/View/Registration.php
@@ -5,29 +5,29 @@
         <title> Registration Log In Form</title>
     </head>
     <body>
-        <form method ="post">
+        <form>
             <table>
                 <tr>
                     <td><p style = 'color: red '> * required field </p></td><br>
                 </tr>
                 <tr>
                     <td> <label for ="Name"> Name: </label></td>
-                    <td> <input type ="text" id = "name" name = "name"> <?php echo $name ?></td>
+                    <td> <input type ="text" id = "name" name = "name"></td>
                     <td> <p style = 'color: red'>*</p> </td>
                 </tr>
                 <tr>
                     <td> <label for ="email"> E-mail: </label></td>
-                    <td> <input type = "email" id="email" name ="email"><?php echo $email ?></td>
+                    <td> <input type = "email" id="email" name ="email"></td>
                     <td> <p style = 'color: red'>*</p> </td>
                 </tr>
                 <tr>
                     <td> <label for ="website"> Website: </label></td>
-                    <td> <input type = "website" id="website" name ="website"><?php echo $website ?></td>
+                    <td> <input type = "url" id="website" name ="website"></td>
                     <td> <p style = 'color: red'>*</p> </td>
                 </tr>
                 <tr>
                     <td> <label for ="comment"> Comment: </label></td>
-                    <td> <input type = "textarea" id="comment" name ="comment"><?php echo $comment ?></td>
+                     <td><textarea name="comment" id="comment"></textarea></td>
                     <td> <p style = 'color: red'>*</p> </td>
                 </tr>    
                 
